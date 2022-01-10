754fdfbd3f96be2530f009e5204d4a45c260f43d
Andrew Ngo <94930359+MiinoSil@users.noreply.github.com>
2022-01-09 23:10:28 -0800

    Working password generator

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ var AlphaUpCase = ["ABCDEFJHIJKLMNOPQRSTUVWXYZ"];
 var SpChar = ["`~!@#$%^&*)(-_=+}{][:?><"];
 var Numbers = ["0123456789"];
 
-
 console.log(AlphaLoCase);
 console.log(AlphaUpCase);
 console.log(SpChar);
 console.log(Numbers);
 
-// Write password to the #password input
-function writePassword() {
+// Function to generate password.
+function generatePassword () {
+  
+  var password = "";
 
   // Prompt user to input length of password between 8 to 128 characters?
   var pwlength = (window.prompt("Please input password length between 8 to 128 character?")); {
@@ -31,25 +32,30 @@ function writePassword() {
 
   // Prompt user to confirm to lowercase characters in password?
   if (cmfLo = (window.confirm("Click OK to include lowercase characters (e.g. a to z) in the password?"))) {
-    availableChars = availableChars.concat(AlphaLoCase); 
+    availableChars += AlphaLoCase;
+    // availableChars = availableChars.concat(AlphaLoCase); 
   }; 
   
   // Prompt user to confirm to include uppercase characters in password?
   if (cmfUp = (window.confirm("Click OK to include uppercase characters (e.g. A to Z) in the password?"))) {
-    availableChars = availableChars.concat(AlphaUpCase);
+    availableChars += AlphaUpCase;
+    // availableChars = availableChars.concat(AlphaUpCase);
   }; 
   
   // Prompt user to confirm to include special characters in password?
   if (cmfSp = (window.confirm("Click OK to include special characters (e.g. !@#$%^&*? and more) in the password?"))) {
-    availableChars = availableChars.concat(SpChar);
+    availableChars += SpChar;
+    // availableChars = availableChars.concat(SpChar);
   }; 
   
   // Prompt user to confirm to include numbers in password?
   if (cmfNm = (window.confirm("Click OK to include numbers (e.g. 0 to 9) in the password?"))) {
-    availableChars = availableChars.concat(Numbers);
+    availableChars += Numbers;
+    // availableChars = availableChars.concat(Numbers);
   }; 
   
   console.log(availableChars);
+  console.log(availableChars.length);
   
   // Loop in the case user does not make a selection for any criteria.
   if (cmfLo + cmfUp + cmfSp + cmfNm === 0) {
@@ -57,21 +63,20 @@ function writePassword() {
     return writePassword();
   };
 
-  //Generate password based on defined function.
-  var password =  generatePassword();
-  function generatePassword () {
-    // Math function to fill in characters based upon confirmed password criteria based upon input password length.
-    for (var i = 0; i < pwlength; i++) {
-      password = password + availableChars.charAt(Math.floor(Math.random() * availableChars.length + 1))
-    } 
-    console.log(password);
-  };
+  // Math function to fill in characters based upon confirmed password criteria based upon input password length.
+  for (var i = 0; i < pwlength; i++) {
+    password += availableChars.charAt(Math.floor(Math.random() * availableChars.length));
+  } return password; 
+};
 
 
-
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
-  
+
 };
 
 // Add event listener to generate button
